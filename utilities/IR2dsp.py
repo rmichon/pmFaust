37ca9d9af2d71141bb0a31fcea59f3df7d4b2ee9
Pierre-Amaury GRUMIAUX <pierreamaury.grumiaux@gmail.com>
2017-02-27 17:19:07 +0100

    add latest version of IR2dsp.py

--- utilities/IR2dsp.py
+++ utilities/IR2dsp.py
@@ -1,87 +1,123 @@
 from __future__ import division
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from sys import argv
 import subprocess
 from scipy.io.wavfile import read
 import peakutils
+import peakutils.plot as pkplt
+import operator
+import argparse
 
-script, sound, model, Nwindow, t = argv
+# Help for use of the script
+parser = argparse.ArgumentParser(description = "IR2dsp.py is a python script that generates a .dsp file from an impulse response file (a .wav file).The impulse response is analyzed in order to rebuild the sound of the vibration of the object based on this impulse response.", epilog = "See http://faust.grame.fr/images/faust-tutorial2.pdf for more information about Faust")
+parser.add_argument("soundFile", type = str, help = "Path of the sound file")
+parser.add_argument("modelName", help = "The name of the model created in the .dsp file")
+parser.add_argument("peakThreshold", help = "Minimum value of peaks in dB (between -infinity and 0)")
+parser.add_argument("peakDistance", help = "Minimum distance between two peaks in Hertz")
+ars = parser.parse_args ()
+# Arguments
+script, soundFile, modelName, peakThreshold, peakDistance = argv
 
-N = int(Nwindow)
-(fs, x2) = read(sound)
-t60 = t
-modelName = model
-x = x2[0:N] #windowing brutal
+# Reading file
+(fs, x) = read(soundFile)
+# Normalizing sound
 x = x/max(x)
 
-#FFT
-fft_size = N
-X = np.abs(np.fft.fft(x, fft_size))
+# FFT
+X = np.abs(np.fft.fft(x))
+X = X/(max(X))
+# computing corresponding frequencies
+time_step = 1/fs
+freqs = np.fft.fftfreq(x.size, time_step)
+idx = np.argsort(freqs)
 
-#computing corresponding frequencies
-time_step = 1 / 44100
-freqs = np.fft.fftfreq(x.size, time_step) 
-idx = np.argsort(freqs) 
+# plot for debug
+# plt.plot(freqs[idx],20*np.log(X[idx]))
+# plt.show()
 
-#detecting peaks
-indexes = peakutils.indexes(X[idx], thres=0.01, min_dist=fft_size/100)
-    
-#plt.plot(freqs[idx], X[idx])
-#plt.show()
+# detecting peaks
+threshold = math.pow(10, float(peakThreshold)/20) # from dB to X unit
+distance = int(peakDistance)/(fs/x.size)
+indexes = peakutils.indexes(X[idx], thres = threshold, min_dist = distance)
 
-#Storing frequencies and modes for each bp filters
+# Storing frequencies and modes for each bp filters
 peaksFreq = []
-peaksMode = []
-for i in indexes:
-    if freqs[idx][i] > 0:
-        peaksFreq.append(freqs[idx][i])
-        peaksMode.append(X[idx][i])
-peaksMode = peaksMode/(max(peaksMode))
+peaksGains = []
+nbOfPeaks = 0
+for i in indexes :
+	if freqs[idx][i] > 0:
+		peaksFreq.append(freqs[idx][i])
+		peaksGains.append(X[idx][i])
+		nbOfPeaks += 1
+peaksGains = peaksGains/(max(peaksGains))
 
-print "peaks frequencies :"
+#Computing t60 values
+peakst60 = []
+for i in range(0, nbOfPeaks) :
+	offset = pow(10, -3/20) # conversion of -3 dB in X unit
+	peakIndex = indexes[len(indexes) - nbOfPeaks + i]
+
+	n = peakIndex
+	while X[idx][n] > (X[idx][peakIndex]*offset):
+		n = n-1
+	a = n
+
+	n = peakIndex
+	while X[idx][n] > (X[idx][peakIndex]*offset):
+		n = n+1
+	b = n
+
+	bandwidth = (b-a)/(fs/x.size) # bandwidth in Hz
+	print bandwidth
+	peakst60.append(6.91/fs/(1-math.exp(-math.pi*bandwidth/fs)))
+
+print " peaks frequencies : "
 print peaksFreq
-print "corresponding gains :"
-print peaksMode
-
+print " corresponding gains : "
+print peaksGains
+print " corresponding t60 : "
+print peakst60
 
 # Writing the dsp file #
-###########################
+# #######################
 file = open(modelName + ".dsp", "w")
-
-file.write("import(\"architecture/pm.lib\");\n")
-file.write("import(\"music.lib\");\n\n")
+file.write("import(\"pm.lib\");\n")
+file.write("import(\"libraries/old/music.lib\");\n\n")
 file.write("pi = 4*atan(1.0);\n")
 file.write("nModes = ")
-file.write(str(len(peaksMode)))
+file.write(str(len(peaksGains)))
 file.write(";\n")
-file.write("eigenValues = ("); #writing the frequencies list
+file.write("modeFrequencies=("); # writing the frequencies list
 k = 0
-for i in peaksFreq :
-    file.write(str(i))
-    if(k+1 < len(peaksMode)):
-        file.write(", ")
-    k += 1
+for i in peaksFreq:
+	file.write(str(i))
+	if (k+1<len(peaksGains)):
+		file.write(",")
+	k += 1
 file.write(");\n");
-file.write("massEigenValues = ("); #writing the masses list
+file.write("massEigenValues=("); # writing the masses list
 k = 0
-for i in peaksMode :
-    file.write(str(i))
-    if(k+1 < len(peaksMode)):
-        file.write(", ")
-    k += 1
+for i in peaksGains:
+	file.write(str(i))
+	if (k+1 < len(peaksGains)):
+		file.write(",")
+	k += 1
 file.write(");\n");
-file.write("modeFreqs = par(i,nModes,sqrt(take(i+1,eigenValues))*2*pi);\n")
-file.write("modeGains = par(i,nModes,take(i+1,massEigenValues));\n")
-file.write("t60 = ")
-file.write(str(t60))
-file.write(";\n\n")
+file.write("t60=("); # writing the t60 list
+k = 0
+for i in peakst60:
+	file.write(str(i))
+	if (k+1 < len(peaksGains)):
+		file.write(",")
+	k += 1
+file.write(");\n");
+file.write("modeFreqs=par(i,nModes,take(i+1, modeFrequencies));\n")
+file.write("modeGains=par(i,nModes,take(i+1, massEigenValues));\n")
+file.write("modeT60 = par(i,nModes,take(i+1,t60));\n")
 file.write(modelName)
-file.write(" = modalModel(nModes,modeFreqs,modeGains,t60);");
-file.write('\n gate = button("gate");')
-file.write('\n process = impulseExcitation(gate) : ' + modelName + ' <: _,_;')
-
+file.write("=modalModel(nModes,modeFrequencies,modeGains,modeT60);");
+file.write('\ngate = button("gate");')
+file.write("\nprocess = impulseExcitation(gate) : " + modelName + " <: _,_; ")
 file.close();
-
-
-
